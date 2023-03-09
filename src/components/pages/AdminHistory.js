bec37d6b148b46901ffbcaf7f1029eb8cccf93ea
Daneswari-Sureddy <daneswarisureddy302@gmail.com>
2023-03-09 18:04:30 +0530

    Modified AdminHistory

--- src/components/pages/AdminHistory.js
+++ src/components/pages/AdminHistory.js
@@ -1,32 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 // import firebase from 'firebase/compat/app';
 import { useNavigate } from "react-router-dom";
+import users from "./userhis.png";
+import workers from "./workhis.jpg";
+import booking from "./booking.png";
 
 const AdminHistory = () => {
-
   const navigate = useNavigate();
-  const toBookingHistory = () =>{
-    let path = '/bookingHistory'; 
+  const toBookingHistory = () => {
+    let path = "/bookingHistory";
     navigate(path);
-  }
-  const toWorkersHistory = () =>{
-    let path = '/workersHistory'; 
+  };
+  const toWorkersHistory = () => {
+    let path = "/workersHistory";
     navigate(path);
-  }
-  const toUsersHistory = () =>{
-    let path = '/usersHistory'; 
+  };
+  const toUsersHistory = () => {
+    let path = "/usersHistory";
     navigate(path);
-  }
+  };
 
   return (
     <div>
       <br></br>
-      <button type = "submit" onClick={toBookingHistory}>Booking History</button><br></br><br></br><br></br>
-      <button type = "submit" onClick={() => toWorkersHistory()}>Workers History</button><br></br><br></br><br></br>
-      <button type = "submit" onClick={() => toUsersHistory()}>Users History</button><br></br>
+      <br></br>
+      <center>
+        <center>
+          <h1>History Details</h1>
+          <br></br>
+        </center>
+        <button
+          type="submit"
+          onClick={toBookingHistory}
+          style={{ borderRadius: "50%", outline: "none" }}
+        >
+          <img
+            src={booking}
+            height="250"
+            width="250"
+            alt="Users"
+            style={{ borderRadius: "50%" }}
+          />
+        </button>
+        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+        <button
+          type="submit"
+          onClick={toUsersHistory}
+          style={{ borderRadius: "50%" }}
+        >
+          <img
+            src={users}
+            height="250"
+            width="250"
+            alt="Users"
+            style={{ borderRadius: "50%" }}
+          />
+        </button>
+        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+        <button
+          type="submit"
+          onClick={toWorkersHistory}
+          style={{ borderRadius: "50%" }}
+        >
+          <img
+            src={workers}
+            height="250"
+            width="250"
+            alt="Users"
+            style={{ borderRadius: "50%" }}
+          />
+        </button>
+        <h1>
+          Booking History &emsp;&emsp;User's History &emsp;&emsp;Worker's
+          History
+        </h1>
+      </center>
     </div>
-  )
-}
+  );
+};
 
-export default AdminHistory
+export default AdminHistory;
